05d4f40c50d1b9802ef3b5238e5920f151ca91f8
RookieTalent <2544472820@qq.com>
2021-02-09 16:38:14 +0800

    first commit

---
+++ guli-coupon/src/test/java/run/lin/app/coupon/GuliCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package run.lin.app.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ guli-ware/src/test/java/run/lin/app/ware/GuliWareApplicationTests.java
@@ -0,0 +1,13 @@
+package run.lin.app.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
